6198d0bc76c451d8e0400094a9920ea3a9dcc0c7
b_liu <b.liu@nki.nl>
2017-03-28 22:06:24 +0200

    add samples app

--- biodataware/config/settings/common.py
+++ biodataware/config/settings/common.py
@@ -52,6 +52,7 @@ LOCAL_APPS = [
     'roles.apps.RolesConfig',
     'groups.apps.GroupsConfig',
     'containers.apps.ContainersConfig',
+    'samples.apps.SamplesConfig'
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
