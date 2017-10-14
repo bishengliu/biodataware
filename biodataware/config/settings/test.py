2e275f6826b0094ae3c9b4e1dcb3195a70c6fe4c
b_liu <b.liu@nki.nl>
2017-10-14 20:18:34 +0200

    add test settings

--- biodataware/config/settings/test.py
+++ biodataware/config/settings/test.py
@@ -19,3 +19,62 @@ INTERNAL_IPS = (
 
 # message level
 MESSAGE_LEVEL = 10  # DEBUG
+
+
+ALLOWED_HOSTS = ['*']
+
+# message level
+MESSAGE_LEVEL = 10  # DEBUG
+
+
+# https://github.com/ottoyiu/django-cors-headers/#configuration
+# cors rest api request
+CORS_ORIGIN_ALLOW_ALL = True
+
+# restful
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication', # token
+        #'helpers.authentication.ExpiringTokenAuthentication', # custom token
+        #'rest_framework.authentication.BasicAuthentication', # login plain password
+        #'rest_framework.authentication.SessionAuthentication', # session on the same server
+    )
+}
+
+
+'''
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'test@gmail.com'
+SERVER_EMAIL = 'test@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'test@gmail.com'
+EMAIL_HOST_PASSWORD = 'test123##'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+'''
+
+SITE_NAME = 'bioku.nl'
+SITE_PROTOCOL = 'http'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
+
+
+# DATABASE
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'", },
+        'NAME': 'bioku_test',
+        'USER': 'bioku',
+        'PASSWORD': 'bioku',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+# for development
+SITE_ID = 1
